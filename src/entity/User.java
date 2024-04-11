4bcd3a4eee239f5b518cd4fce3e6ed86c6a59255
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-04-11 19:59:45 +0300

    created user entity

---
+++ src/entity/User.java
@@ -0,0 +1,65 @@
+package entity;
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private Role role;
+
+    public enum Role{
+        ADMIN,
+        EMPLOYEE
+    }
+
+    public User() {
+    }
+
+    public User(int id, String name, String password, Role role) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
