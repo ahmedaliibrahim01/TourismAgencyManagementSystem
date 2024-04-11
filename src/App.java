935895ea9e62bb9dabc791d5448fadd64907e06a
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-04-11 19:14:34 +0300

    database connection made

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import core.Db;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Tourism Agency System");
+        Db.getInstance();
     }
 }
