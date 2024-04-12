d913954053db603d874998873e8d06e03782756a
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-04-12 17:14:50 +0300

    created userView and edited its components

--- src/view/UserManagementView.java
+++ src/view/UserManagementView.java
@@ -6,6 +6,7 @@ import entity.User;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class UserManagementView extends Layout{
@@ -25,6 +26,8 @@ public class UserManagementView extends Layout{
     private JPanel pnl_filter;
     private JPanel pnl_table_user;
     private JScrollPane scrl_table;
+    private JPopupMenu user_menu;
+    private Object[] col_model;
     private User user;
     private DefaultTableModel tmdl_users = new DefaultTableModel();
     private UserManager userManager;
@@ -39,25 +42,46 @@ public class UserManagementView extends Layout{
         }
         this.lbl_welcome.setText("Welcome :  " + Helper.firstWordUpper(this.user.getName()));
 
-        // User List
-        loadUsersTable(null);
-        loadUsersFilter1();
+        // User Managemenet
+        loadUsersTable();
+        //loadUserComponent();
+        this.tbl_users.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int selectedRow = tbl_users.rowAtPoint(e.getPoint());
+                tbl_users.setRowSelectionInterval(selectedRow,selectedRow );
+            }
+        });
+        this.user_menu = new JPopupMenu();
+        this.user_menu.add("Add").addActionListener(e -> {
+            UserView userView = new UserView(null);
+        });
+        this.user_menu.add("Update").addActionListener(e -> {
+            UserView userView = new UserView(this.user);
+        });
+        this.user_menu.add("Delete").addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to do this ?", "Delete", JOptionPane.YES_NO_OPTION);
+            if (result == JOptionPane.YES_OPTION) {
+                // Silme işlemini burada gerçekleştir
+                // Örneğin: deleteItem();
+            }
+        });
+
+        this.tbl_users.setComponentPopupMenu(user_menu);
 
         // LogOut
         logout();
 
     }
 
-    private void loadUsersTable(ArrayList<Object[]> userList) {
+    private void loadUsersTable() {
         Object[] col_user_list = {"ID", "Name", "Password", "Role"};
-        if (userList == null){
-            userList = this.userManager.getForTable(col_user_list.length, this.userManager.findAll());
-        }
-        createTable(this.tmdl_users, this.tbl_users, col_user_list, userList);
+        ArrayList<Object[]> userList = this.userManager.getForTable(col_user_list.length);
+        this.createTable(this.tmdl_users, this.tbl_users, col_user_list, userList);
     }
 
 
-    public void loadUsersFilter1(){
+    public void loadUsersFilter(){
         this.cmbx_user_filter.addItem("ALL USERS");
         this.cmbx_user_filter.addItem("ADMIN");
         this.cmbx_user_filter.addItem("EMPLOYEE");
@@ -70,6 +94,4 @@ public class UserManagementView extends Layout{
             LoginView loginView = new LoginView();
         });
     }
-
-
 }
---
+++ src/view/UserView.java
@@ -0,0 +1,53 @@
+package view;
+
+import business.UserManager;
+import core.Helper;
+import entity.User;
+
+import javax.swing.*;
+
+public class UserView extends Layout{
+
+    private JPanel container;
+    private JTextField txtf_username;
+    private JTextField txtf_password;
+    private JButton btn_user_save;
+    private JLabel lbl_update_new;
+    private JLabel lbl_username;
+    private JLabel lbl_password;
+    private JLabel lbl_role;
+    private JComboBox cmbx_role;
+    private UserManager userManager;
+    private User user;
+
+    public UserView(User user){
+        this.userManager = new UserManager();
+        this.user = user;
+        this.add(container);
+        this.guiInitilaze(300, 300);
+
+        if (user != null) {
+            this.txtf_username.setText(user.getName());
+            this.txtf_password.setText(user.getPassword());
+            this.cmbx_role.setToolTipText("ADMIN");
+        }
+        btn_user_save.addActionListener(e -> {
+            if (Helper.isFieldEmpty(this.txtf_username) || Helper.isFieldEmpty(this.txtf_password)) {
+                Helper.showMsg("fill");
+            } else {
+                boolean result = true;
+                if (this.user == null) {
+                    User obj = new User(txtf_username.getText(),txtf_password.getText());
+                    result = this.userManager.save(obj);
+                }
+
+                if (result) {
+                    Helper.showMsg("done");
+                    dispose();
+                } else {
+                    Helper.showMsg("error");
+                }
+            }
+        });
+    }
+}
